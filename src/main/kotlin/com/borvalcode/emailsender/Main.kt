eb3f60cca9d62a25ffe8becc7896474020c187cc
Borja Valdivielso <borja.valdivielso@multisafepay.com>
2022-01-30 23:42:32 +0100

    Update Main

--- src/main/kotlin/com/borvalcode/emailsender/Main.kt
+++ src/main/kotlin/com/borvalcode/emailsender/Main.kt
@@ -1,37 +1,45 @@
 package com.borvalcode.emailsender
 
+import arrow.core.flatMap
+import com.borvalcode.emailsender.domain.`or else`
 import com.borvalcode.emailsender.domain.dto.Email.Body.Part.Html
 import com.borvalcode.emailsender.domain.dto.Email.Body.Part.Text
 import com.borvalcode.emailsender.domain.dto.Email.Companion.email
 import com.borvalcode.emailsender.domain.dto.EmailError
+import com.borvalcode.emailsender.domain.get
 import com.borvalcode.emailsender.domain.handle
-import com.borvalcode.emailsender.infrastructure.DefaultEmailSender
-import com.borvalcode.emailsender.infrastructure.`or else`
-import com.borvalcode.emailsender.infrastructure.get
+import com.borvalcode.emailsender.domain.send
+import com.borvalcode.emailsender.infrastructure.EmailSender
 
 fun main() {
 
     val inputUrl = "input"
 
     // Safe way of instantiating an EmailSender
-    val defaultSender = DefaultEmailSender.of(host = "localhost", port = 25)
+    val defaultSender = EmailSender.of(host = "localhost", port = 25)
 
     // Non-safe way of instantiating an EmailSender: if you ensure the url is [host]:[port], you can use method get()
-    val sender = (DefaultEmailSender on "localhost:25").get()
+    val sender = (EmailSender on "localhost:25").get()
 
     // Non-safe way of instantiating an EmailSender: if you can't ensure the url is well-formed, you can declare a default object
-    val orDefaultSender = DefaultEmailSender on inputUrl `or else` defaultSender
+    val orDefaultSender = EmailSender on inputUrl `or else` defaultSender
 
     // Non-safe way of instantiating an EmailSender: if you can't ensure the url is well-formed, you can declare a routine if parsing fails
-    val orThrowSender = DefaultEmailSender on inputUrl `or else` { throw RuntimeException(it.message, it.cause) }
+    val orThrowSender = EmailSender on inputUrl `or else` { throw RuntimeException(it.message, it.cause) }
 
     ////////////////////
 
     // The simplest email
     val emailMessage = email from "sender@email.com" to "receiver@email.com" subject "Hello" body "Hello World"
 
+    // Email is also an either, so you can work with it
+
+
     // Email with many receivers
-    val emailWithReceivers = email from "sender@email.com" to setOf("receiver@email.com", "another@gmail.com") subject "Hello" body "Hello World"
+    val emailWithReceivers = email from "sender@email.com" to setOf(
+        "receiver@email.com",
+        "another@gmail.com"
+    ) subject "Hello" body "Hello World" `or else` { throw RuntimeException(it.message, it.cause) }
 
     // Email with complex body
     val emailWithComplexBody = (email from "sender@email.com" to "receiver@email.com" subject "Hello"
@@ -46,6 +54,13 @@ fun main() {
             `attached files` setOf("File1.txt", "File2.txt")
             body (Html of "<html></html>"))
 
+    // Email is also non-safe
+    email from "from" to "to" subject "subject" body "body" `or else` {
+        throw RuntimeException(
+            it.message,
+            it.cause
+        )
+    }
 
     ////////////////////
 
@@ -57,6 +72,29 @@ fun main() {
     (orThrowSender send emailWithComplexBody)
         .handle(onSuccess, onError)
 
+    // Single line integration
+
+    // Wrong url -> Error sending email: Pattern must be [host]:[port]
+    (EmailSender on "wrong_url")
+        .flatMap { it send (email from "from@from.com" to "to@to.com" subject "subject" body "body") }
+        .handle(success = { println("Mail sent!") }, error = { println("Error sending email: ${it.message}") })
+
+    // Wrong email address -> Error sending email: Invalid email address: from
+    (EmailSender on "localhost:25")
+        .flatMap { it send (email from "from" to "to@to.com" subject "subject" body "body") }
+        .handle(success = { println("Mail sent!") }, error = { println("Error sending email: ${it.message}") })
+
+    // Error when sending -> Error sending email: Error sending email
+    (EmailSender on "localhost:25")
+        .flatMap { it send (email from "from@from.com" to "to@to.com" subject "subject" body "body") }
+        .handle(success = { println("Mail sent!") }, error = { println("Error sending email: ${it.message}") })
+
+    // Simplest one line
+    (EmailSender.of("host", 25) send (email from "from@from.com" to "to@to.com" subject "subject" body "body"))
+        .handle(success = { println("Mail sent!") }, error = { println("Error sending email: ${it.message}") })
+
+    ((EmailSender on "wrong_url") send (email from "from@from.com" to "to@to.com" subject "subject" body "body"))
+        .handle(success = { println("Mail sent!") }, error = { println("Error sending email: ${it.message}") })
 }
 
 private val onError: (EmailError) -> Unit = { println("Error sending email: $it") }
